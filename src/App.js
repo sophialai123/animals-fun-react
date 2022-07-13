833bbeda98bb944b6ee9d34e2b826688bbc3aba6
Sophia <sophia852686@gmail.com>
2022-07-13 15:00:42 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import animals from './components/animals';
+import ocean from './images/ocean.png'
+
+
+const title = '';
+const showBackground = false;
+const background = <img
+  className="background"
+  alt="ocean"
+  src={ocean}
+/>
+
+//push all the image in the array
+let images = [];
+for (const animal in animals) {
+  // images.push(animals[animal].image)
+  images.push(
+    <img
+      key={animal}
+      className='animal'
+      alt={animal}
+      src={animals[animal].image}
+      aria-label={animal}
+      role='button'
+      onClick={displayFact}
+    />
+  )
+}
+
+function displayFact(e) {
+  let selectedAnimal = e.target.alt;
+  let animalInfo = animals[selectedAnimal];
+  const optionIndex = Math.floor(Math.random() * animalInfo.facts.length);
+  const funFact = animalInfo.facts[optionIndex];
+
+  //display on the screen
+  document.getElementById('fact').innerHTML = funFact
+
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      {/*if title is false the will render on the right side*/}
+      <h1>{title || 'Click an animal for a fun fact'}</h1>
+      {/* if showBackground is true, the background will show */}
+      {showBackground && background}
+      <p id='fact'></p>
+      <div className="animals">
+        {images}
+      </div>
     </div>
+
   );
 }
 
 export default App;
+
---
+++ src/components/animals.js
@@ -0,0 +1,20 @@
+import dolphin from "../images/dolphin.png"
+import starfish from '../images/starfish.png';
+import lobster from '../images/lobster.png'
+
+const animals = {
+  dolphin: {
+    image: dolphin,
+    facts: ['Dolphins have been shown to give distinct names to each other!', 'Dolphins are known to display their own culture!', 'Dolphins have two stomachs!']
+  },
+  lobster: {
+    image: lobster,
+    facts: ['Lobsters taste with their legs!', 'Lobsters chew with their stomachs!', 'Lobsters can live as long as 100 years.']
+  },
+  starfish: {
+    image: starfish,
+    facts: ['Starfish can have up to 40 arms!', 'Starfish have no brain and no blood!', 'Starfish can regenerate their own arms!']
+  }
+};
+
+export default animals;
